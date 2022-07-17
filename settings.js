a79082bebfb1595ec768e8d3bba71a47a3db3b34
Amithak10 <101508177+Amithak10@users.noreply.github.com>
2022-07-17 14:35:56 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -54,9 +54,9 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['916909137213'] //ur owner number
+global.vcardowner = ['917561852787'] //ur owner number
 global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
+global.ytname = "YT: RINGTONE FREEK" //ur yt chanel name
 global.socialm = "GitHub: DGXeon" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
